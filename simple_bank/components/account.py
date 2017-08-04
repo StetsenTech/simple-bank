601f383087cb4e6162c873ad667861a3f9bad81f
Brian Rose <stetsentech@gmail.com>
2017-08-04 19:58:58 -0400

    Starts refactor for account component
    - Updating cash goes through one method
    - Updates logic for withdraw
    - Adds ATM and Overdraft fee options
    - Updates docstrings
    - Adds some TODOs

--- simple_bank/components/account.py
+++ simple_bank/components/account.py
@@ -8,6 +8,9 @@ class Account(object):
         self.cash = 0
         self.dividend_rate = 0
         self.transactions = []
+        self.atm_fee = 0
+        self.overdraft_protection = False
+        self.overdraft_fee = 0
 
     def __add__(self, o_acct):
         """Find the differences between accounts"""
@@ -31,24 +34,36 @@ class Account(object):
         Args:
             amount(float): Amount of cash to deposit
         """
-        self.cash += amount
+        return self._adjust_cash(amount)
 
-    def withdraw_cash(self, amount):
+    def withdraw_cash(self, amount, atm=False, force=False):
         """Withdraws cash from account
 
         Args:
             amount(float): Amount of cash to withdraw
         """
-        self.cash += amount
+        if not force and (self.cash <= 0 or self.cash <= amount):
+            # @TODO: Create exception for Withdraw errors
+            raise Exception("There isn't enough cash to withdraw specified amount.")
 
-    def apply_dividend(self, dividend):
+        if self.cash - amount < 0 and self.overdraft_protection:
+            self.cash -= self.overdraft_fee
+        else:
+            # @TODO: Create exception for overdraft errors
+            raise Exception("Unable to overdraft account.")
+
+
+        return self._adjust_cash(-amount)
+
+
+    def apply_dividend(self):
         """Pays dividend
 
         Args:
             symbol(string): Name of shares
             dividend(float): Dividend issued
         """
-        self.cash += dividend
+        raise NotImplementedError("Applying dividend to account is not implemented.")
 
     def apply_fee(self, fee):
         """Fees owed
@@ -56,4 +71,25 @@ class Account(object):
         Args:
             amount(float): Amount of fee
         """
-        self.cash -= fee
+        return self._adjust_cash(-fee)
+
+    def _add_transaction(self, t_name, t_date, t_amount):
+        """Adds transtion to history
+
+        Args:
+            t_name(string): Name/Details of transction
+            t_date(string): Date of transction
+            t_amount(float): Transaction amount
+        """
+        pass
+
+    def _adjust_cash(self, amount):
+        """Handles changing amount of cash in current account
+
+        Args:
+            amountamount(float): 
+        """
+        
+        self.cash += amount
+
+        return self.cash
